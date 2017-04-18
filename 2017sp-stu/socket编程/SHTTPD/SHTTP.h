d79aff9abb1eb23f97bae6975f11d490207513e8
FerrisChan <ferris.chan@foxmail.com>
2017-04-18 21:36:24 +0800

    定义一些数据结构和增加线程函数

--- 2017sp-stu/socket编程/SHTTPD/SHTTP.h
+++ 2017sp-stu/socket编程/SHTTPD/SHTTP.h
@@ -4,5 +4,52 @@
 
 #ifndef SHTTPD_SHTTP_H
 #define SHTTPD_SHTTP_H
+  #include <stdio.h>
+  #include <sys/socket.h>
+  #include <netinet/in.h>
+  #include <string.h>
+  #include <signal.h>
+  #define  K 1024
+
+
+/*
+ * 需要数据结构类型的声明
+ *
+*/
+
+  //线程的状态值
+  enum{WORKER_INITED, WORKER_RUNNING,
+      WORKER_DETACHING, WORKER_DETACHED,WORKER_IDEL};
+  /*
+   * 管理线程的状态
+   */
+  struct worker_opts{
+      pthread_t th;  //线程的ID号
+      int flags;     // 线程的状态
+      pthread_mutex_t mutex; //线程任务互斥
+      struct worker_ctl *work;  // 本线程的总控结构
+  };
+
+  /*
+   * 管理客户端请求的状态和值
+   */
+  struct workr_conn{
+      char dreq[16*K];  // 请求缓冲区大小
+      char dres[16*K];  //响应缓冲区大小
+      int  cs;          // client  的socket描述符
+      int  to;          //client 链接超时
+      struct worker_ctl *work ;//本线程的总控结构
+      //调用相应函数结构
+//      struct conn_response con_res;
+//      struct conn_request con_req;
+ };
+
+  struct worker_ctl {
+     struct worker_opts opts;
+     struct worker_conn conn;
+ };
+
+
 
 #endif //SHTTPD_SHTTP_H
+
--- 2017sp-stu/socket编程/SHTTPD/thread_set.c
+++ 2017sp-stu/socket编程/SHTTPD/thread_set.c
@@ -2,3 +2,38 @@
 // Created by ferris on 17-4-15.
 //
 
+#include <pthread.h>
+#include <malloc.h>
+#include "SHTTP.h"
+
+//参数初始化
+static int MaxClient = 500;  //最大线程数
+static int workersum = 0; //工作线程的数量
+static struct worker_ctl *wctls = NULL ; //线程选项
+pthread_mutex_t thread_init = PTHREAD_MUTEX_INITIALIZER; //init 线程锁
+static int WORKER_ISSTATUS(int status);
+static void Worker_Init();
+static int Worker_Add(int i);
+static void Worker_Delete(int i);
+static void Worker_Destory();
+
+// 线程初始化
+static void Worker_Init()
+{
+    printf("==> Worker_Init is working \n");
+    int i =0 ;
+    //初始化总控参数
+    wctls = (struct worker_ctl*)malloc(sizeof(struct worker_ctl)*MaxClient);
+    memset(wctls,0,sizeof(*wctls)*MaxClient);
+
+
+
+}
+
+int Worker_ScheduleRun(int ss)
+{
+    printf("==> Worker_ScheduleRun is working \n");
+    struct sockaddr_in client;
+    socklen_t len = sizeof(client);
+    Worker_Init();
+}
